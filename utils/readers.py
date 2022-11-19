0cbf8483f32c8cc93e6de8e31a3bf76a644a598e
hung-chi <iewis01118@gmail.com>
2022-11-19 11:37:42 +0100

    Added reader for acqurior_uk

--- utils/readers.py
+++ utils/readers.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 MA_ACQUIRING_FILE = r'../data/MA_acquiring.csv'
 QUARTERLY_FILE = r'../data/quarterly.csv'
+ACQURIOR_FILE = r'../data/acquiror_uk.csv'
 
 
 def clean_df(df, drop_empty):
@@ -23,4 +24,11 @@ def read_quarterly(drop_empty=True):
     """Read quarterly data"""
     df = pd.read_csv(QUARTERLY_FILE)
     return clean_df(df, drop_empty)
+
+
+def read_acquiror_uk():
+    """Read acquiror"""
+    df = pd.read_csv(ACQURIOR_FILE)
+    df['Date'] = pd.to_datetime(df['Date'], format='%d.%m.%y')
+    return df
 
